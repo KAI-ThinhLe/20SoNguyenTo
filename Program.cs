8eb36bdf52592e4e75dbf48149640b621cfeb0f8
unknown <legiathinh0508@gmail.com>
2024-08-16 20:57:12 +0700

    20SNT

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int count = 0;
+        int num = 2;
+
+        while (count < 20)
+        {
+            bool isPrime = true;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.Write(num + " ");
+                count++;
+            }
+
+            num++;
+        }
+    }
+}
